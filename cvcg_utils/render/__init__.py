a437200c0137ac180ccf83604f2c3e8b3035c2a0
trisst <jsnln>
2025-04-28 12:06:48 +0800

    modified:   cvcg_utils/render/__init__.py

--- cvcg_utils/render/__init__.py
+++ cvcg_utils/render/__init__.py
@@ -1,3 +1,3 @@
-from .camera import UnifiedCamera, DRTKCamera, BatchDRTKCamera, NvdiffrecmcCamera
-from .drtk_renderer import render_drtk_uv_textured, render_drtk_uv_textured, render_drtk_vert_attr, render_drtk_face_attr, render_drtk_shaded
-from .gs_renderer import render_gs
+# from .camera import UnifiedCamera, DRTKCamera, BatchDRTKCamera, NvdiffrecmcCamera
+# from .drtk_renderer import render_drtk_uv_textured, render_drtk_uv_textured, render_drtk_vert_attr, render_drtk_face_attr, render_drtk_shaded
+# from .gs_renderer import render_gs
