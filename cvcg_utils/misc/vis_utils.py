1ce9b71850ecd1491f670d7f8985c378bdbdb8e7
trisst <jsnln>
2025-05-15 15:35:20 +0800

    new file:   cvcg_utils/misc/vis_utils.py

---
+++ cvcg_utils/misc/vis_utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def gen_camera_visualization(K: np.ndarray, c2w: np.ndarray, H: int, W: int, cone_height: float, mode: str):
+    """
+    all in opencv format
+    K: 3x3
+    c2w: 4x4 
+    """
+    uv_corners_homog = np.array([
+        [0, 0, 1.],
+        [H, 0, 1.],
+        [H, W, 1.],
+        [0, W, 1.],
+    ])
+    corners_cam = np.einsum('ij,nj->ni', np.linalg.inv(K), uv_corners_homog) * cone_height
+    corners_world = np.einsum('ij,nj->ni', c2w[:3, :3], corners_cam) + c2w[:3, 3]
+
+    verts = np.pad(corners_world, ((1,0), (0,0)), constant_values=0)   # [5, 3]
+    
+    if mode == 'line':
+        faces = np.array([
+            [0, 1, 0],
+            [0, 2, 0],
+            [0, 3, 0],
+            [0, 4, 0],
+            [1, 2, 1],
+            [2, 3, 2],
+            [3, 4, 3],
+            [4, 1, 4],
+        ])
+    else:
+        raise NotImplementedError(f"mode {mode} is not supported")
+    
+    return verts, faces
