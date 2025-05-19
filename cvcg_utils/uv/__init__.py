3e39d3a34894172cb4bdc54a663a227de5beb053
trisst <jsnln>
2025-05-19 15:20:08 +0800

    modified:   cvcg_utils/uv/__init__.py

--- cvcg_utils/uv/__init__.py
+++ cvcg_utils/uv/__init__.py
@@ -1,2 +1,2 @@
-from .uv_module import face_attr_to_uv, vert_attr_to_uv, convert_edge_uv_to_uv_mesh, get_uv_assets, sample_texture_by_uv
-from .posmap2normal import MaskedPosmap2Normal
+# from .uv_module import face_attr_to_uv, vert_attr_to_uv, convert_edge_uv_to_uv_mesh, get_uv_assets, sample_texture_by_uv
+# from .posmap2normal import MaskedPosmap2Normal
