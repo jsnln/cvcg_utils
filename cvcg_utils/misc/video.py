a39d3c060be99626bfde40547d654b94c2fbcae6
trisst <jsnln>
2025-08-26 19:45:56 +0800

    modified:   ../../cvcg_utils/misc/video.py

--- cvcg_utils/misc/video.py
+++ cvcg_utils/misc/video.py
@@ -2,5 +2,5 @@ from warnings import warn
 
 warn("[Deprecation Warning] Imports from `cvcg_utils.misc.video` is deprecated. Kindly replace them with cvcg_utils.video")
 
-from cvcg_utils.video import write_video, read_video_rgb
+from cvcg_utils.video import write_video, read_video
 
