bbbbe9f4c12f2f23b255b8cb6593cda43282ba40
jsnln <jsnln>
2025-09-11 11:45:28 +0800

    modified:   cvcg_utils/render/camera.py

--- cvcg_utils/render/camera.py
+++ cvcg_utils/render/camera.py
@@ -11,6 +11,40 @@ except:
     torch_has_nn_Buffer = False
 
 
+def K_from_fov(fov_x, fov_y, fov_mode, H, W):
+    assert fov_mode in ['deg', 'rad'], f'param fov_mode must be either deg or rad, now it is {fov_mode}'
+    if (fov_x is None) and (fov_y is None):
+        raise NotImplementedError("Proivde either (fov_x, fov_y) or K")
+    elif fov_y is None:
+        if fov_mode == 'deg':
+            fov_x = fov_x / 180 * math.pi
+        f_x = W / (2 * math.tan(fov_x / 2))
+        f_y = f_x
+
+    elif fov_x is None:
+        if fov_mode == 'deg':
+            fov_y = fov_y / 180 * math.pi
+        f_y = H / (2 * math.tan(fov_y / 2))
+        f_x = f_y
+    
+    else:
+        if fov_mode == 'deg':
+            fov_x = fov_x / 180 * math.pi
+            fov_y = fov_y / 180 * math.pi
+        f_x = W / (2 * math.tan(fov_x / 2))
+        f_y = H / (2 * math.tan(fov_y / 2))
+    
+    c_x = W / 2
+    c_y = H / 2
+
+    K = np.eye(3)
+    K[0,0] = f_x
+    K[1,1] = f_y
+    K[0,2] = c_x
+    K[1,2] = c_y
+
+    return K
+
 def focal2fov(focal, pixels):
     return 2 * math.atan(pixels / (2*focal))
     
