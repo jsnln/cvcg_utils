081ac7288b93981df59e2e6fcee1aac872788fd1
jsnln <jsnln>
2025-11-03 15:02:00 +0800

    modified:   cvcg_utils/render/plucker.py

--- cvcg_utils/render/plucker.py
+++ cvcg_utils/render/plucker.py
@@ -168,3 +168,74 @@ def refine_KRT_with_known_focal(K, R, T, focal, H, W):
     new_T = - new_R @ cam_center
 
     return new_K, new_R, new_T
+
+
+
+
+def solve_PnP(xyz_list: np.ndarray, uv1_list: np.ndarray):
+    """
+    xyz_list: [N_rays, 3]
+    uv1_list: [N_rays, 3] (homogeneous)
+    """
+    xyz_list_homog = np.pad(xyz_list_homog, ((0,0), (0,1)), constant_values=1)  # [N, 4]
+    
+    def matvec(P: np.ndarray):
+        """
+        P: [12,] <=> [3, 4], full projection matrix
+        """
+        P = P.reshape(3, 4)
+        Pxyz = np.einsum('ij,nj->ni', P[:3, :3], xyz_list) + P[:3, 3]
+        BP = np.cross(uv1_list, Pxyz, axis=-1)    # [Nrays, 3]
+
+        return BP.reshape(-1)
+    
+    def rmatvec(r: np.ndarray):
+        """
+        r: [N_rays*3]
+        """
+        r = r.reshape(-1, 3)    # [N, 3]
+        m_uv1_cross_r = -np.cross(uv1_list, r, axis=-1)         # [N, 3]
+        BTr = np.einsum('ni,nj->nij', m_uv1_cross_r, xyz_list_homog).sum(0)  # [3, 4]
+
+        return BTr.reshape(-1)
+    
+    raymap2P_op = LinearOperator((xyz_list.shape[0]*3, 12), matvec=matvec, rmatvec=rmatvec)
+    # (u, s, vh) = svds(raymap2P_op, k=9, maxiter=100, solver='propack', return_singular_vectors=False)
+    # arpack has really bad convergence, use propack instead
+    (_, ss, vhs) = svds(raymap2P_op, which='SM', k=1, return_singular_vectors='vh')
+    vhs = vhs.reshape(3, 4)
+    
+    # step 0: rectify overall orientation
+    # NOTE both vhs and -vhs are valid singular vectors
+    # we need the one with positive determinant because det(KR) > 0
+    if np.linalg.det(vhs[:3, :3]) < 0:
+        vhs = -vhs
+
+    K_prelim_solved, R_prelim_solved = scipy.linalg.rq(vhs[:3, :3])
+    RT_prelim_solved = np.linalg.inv(K_prelim_solved) @ vhs
+    assert np.linalg.det(R_prelim_solved) > 0
+    # NOTE QR result is not usable yet
+    # K is not usable here due to scale ambiguity
+    # R may have still a wrong orientation
+
+    # NOTE step 1: further rectify orientation
+    if K_prelim_solved[0,0] < 0:    # need to flip first row of R
+        K_prelim_solved[:, 0] *= -1
+        RT_prelim_solved[0] *= -1
+
+    if K_prelim_solved[1,1] < 0:    # need to flip second row of R
+        K_prelim_solved[:, 1] *= -1
+        RT_prelim_solved[1] *= -1
+    
+    if np.linalg.det(RT_prelim_solved[:3, :3]) < 0:    # need to flip third row of R
+        K_prelim_solved[:, 2] *= -1
+        RT_prelim_solved[2] *= -1
+
+    # NOTE step 2: rectify scale
+    K_prelim_solved /= K_prelim_solved[2,2] # 
+    
+    K = K_prelim_solved
+    R = RT_prelim_solved[:3, :3]
+    T = RT_prelim_solved[:3, 3]
+
+    return K, R, T
